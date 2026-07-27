ecfaa1d4387238427028350e4ed905c015873985
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-27 21:21:04 +0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,23 @@
+# Password Generator
+import random
+import string
 
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# Example usage
+length = int(input("Enter password length: "))
+print("Generated Password:", generate_password(length))
+
+
+import secrets
+# import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(secrets.choice(characters) for _ in range(length))
+
+length = int(input("Enter password length: "))
+print("Generated Password:", generate_password(length))
