5a6684500a9279dcf1d4bd547079762188be5d7e
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-29 14:35:26 +0500

    Update password_generator.cpp

--- password_generator.cpp
+++ password_generator.cpp
@@ -1,1 +1,39 @@
+// Password Generator
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+
+string generatePassword(int length) {
+
+    string characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789"
+        "!@#$%^&*()_-+=<>?/{}[]";
+
+    string password = "";
+
+    for (int i = 0; i < length; i++) {
+        int index = rand() % characters.length();
+        password += characters[index];
+    }
+
+    return password;
+}
+
+int main() {
+
+    srand(time(0));
+
+    int length;
+
+    cout << "Enter password length: ";
+    cin >> length;
+
+    cout << "Generated Password: " << generatePassword(length) << endl;
+
+    return 0;
+}
