6314e6464d65627b94247a7b991b6e5521b32ae5
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-29 14:39:37 +0500

    Update password_generator.js

--- password_generator.js
+++ password_generator.js
@@ -1,1 +1,19 @@
 // Password Generator
+function generatePassword(length = 12) {
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?/{}[]";
+
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomArray = new Uint32Array(1);
+        crypto.getRandomValues(randomArray);
+
+        const index = randomArray[0] % characters.length;
+        password += characters[index];
+    }
+
+    return password;
+}
+
+console.log("NEW PASSWORD: " + generatePassword(12));
